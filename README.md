5366dbb0a5f5ccd7ff72b6a676bb770af8adb410
amanda <bestilyq@gmail.com>
2025-04-26 09:15:59 +0800

    增加环境依赖安装操作

--- README.md
+++ README.md
@@ -1,40 +1,21 @@
 # LatentSync
 Custom Latentsync from bytedance/Latentsync
 
+## 0、初始化系统环境（可选）
+```bash
+./init_env.sh
+```
+
 ## 1、下载代码
 ```bash
 git clone --recursive https://github.com/bestilyq/LatentSync.git
 ```
 
-## 2、下载模型文件，使用hfd加速下载
-### 下载hfd，添加可执行权限
-```bash
-wget https://hf-mirror.com/hfd/hfd.sh
-chmod +x hfd.sh
-```
-
-### 安装hfd依赖【非root用户需要sudo】
-```bash
-apt install -y aria2 jq
-```
-
-### 下载模型
-```bash
-./hfd.sh ByteDance/LatentSync-1.5 --include whisper/tiny.pt latentsync_unet.pt --local-dir checkpoints
-```
-
-## 3、安装Python环境
-### 确认cuda版本
-```bash
-nvcc --version
-```
-**注意：** 如果cuda版本为11.x，需要将requirements.txt中的onnxruntime-gpu改为onnxruntime-gpu==1.18.0
-
-### 创建python虚拟环境，安装项目依赖
+## 2、创建python虚拟环境，安装项目依赖
 ```bash
 source setup_env.sh
 ```
-### 运行gradio主程序
+## 3、运行gradio主程序
 ```bash
 export PYTHONPATH=$PYTHONPATH:./LatentSync
 python gradio_app.py
@@ -42,10 +23,4 @@ python gradio_app.py
 或者
 ```bash
 ./launch.sh
-```
-
-### *如果创建共享链接失败则执行下面命令后再运行gradio程序*
-```bash
-wget https://cdn-media.huggingface.co/frpc-gradio-0.3/frpc_linux_amd64 -O ~/.cache/huggingface/gradio/frpc/frpc_linux_amd64_v0.3
-chmod +x ~/.cache/huggingface/gradio/frpc/frpc_linux_amd64_v0.3
 ```
--- setup_env.sh
+++ setup_env.sh
@@ -8,7 +8,7 @@ conda activate latentsync
 conda install -y -c conda-forge ffmpeg
 
 # Python dependencies
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 # OpenCV dependencies
 if [ "$(id -u)" -eq 0 ]; then
@@ -16,3 +16,39 @@ if [ "$(id -u)" -eq 0 ]; then
 else
     sudo apt -y install libgl1
 fi
+
+# 获取CUDA版本
+CUDA_VERSION=$(nvcc --version | grep "release" | awk '{print $6}' | cut -d',' -f1 | cut -d'V' -f2)
+echo "CUDA Version: $CUDA_VERSION"
+
+# 如果CUDA版本低于12.0，修改临时文件中的onnxruntime-gpu版本
+if [[ "$(echo $CUDA_VERSION | cut -d'.' -f1)" -lt 12 ]]; then
+    # 判断是否为macOS
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        SED_INPLACE="sed -i ''"
+    else
+        SED_INPLACE="sed -i"
+    fi
+    # 定义临时文件
+    TEMP_FILE=".requirements.txt"
+    # 复制requirements.txt到临时文件
+    cp requirements.txt "$TEMP_FILE"
+    # 修改临时文件中的onnxruntime-gpu版本
+    $SED_INPLACE "s/onnxruntime-gpu==1.21.0/onnxruntime-gpu==1.18.0/" "$TEMP_FILE"
+    # 使用临时文件安装依赖
+    pip install -r "$TEMP_FILE"
+    # 删除临时文件
+    rm "$TEMP_FILE"
+else
+    pip install -r requirements.txt
+fi
+
+# Download the checkpoints required for inference use hfd
+if [ "$(id -u)" -eq 0 ]; then
+    apt install -y aria2 jq
+else
+    sudo apt install -y aria2 jq
+fi
+wget https://hf-mirror.com/hfd/hfd.sh -O hfd.sh
+chmod +x hfd.sh
+./hfd.sh ByteDance/LatentSync-1.5 --include whisper/tiny.pt latentsync_unet.pt --local-dir checkpoints

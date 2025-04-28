686a1149e2febafd394543bc00230fca565aace8
amanda <bestilyq@gmail.com>
2025-04-28 09:09:45 +0800

    update web page infomation

--- gradio_app.py
+++ gradio_app.py
@@ -113,19 +113,19 @@ def create_args(
 
 
 # Create Gradio interface
-with gr.Blocks(title="LatentSync demo") as demo:
+with gr.Blocks(title="AvatarX") as demo:
     gr.Markdown(
     """
-    <h1 align="center">LatentSync</h1>
-
+    <h1 align="center">AvatarX</h1>
     <div style="display:flex;justify-content:center;column-gap:4px;">
-        <a href="https://github.com/bytedance/LatentSync">
+        <a href="https://github.com/bestilyq/AvatarX">
             <img src='https://img.shields.io/badge/GitHub-Repo-blue'>
         </a> 
         <a href="https://arxiv.org/abs/2412.09262">
             <img src='https://img.shields.io/badge/arXiv-Paper-red'>
         </a>
     </div>
+    <h2 align="center">A digital human build with LatentSync and F5-TTS</h2>
     <style> video { max-height: 500px; } </style>
     """
     )

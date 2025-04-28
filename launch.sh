5b4f208978496712c1e1f554a02acbcbb85347a8
amanda <bestilyq@gmail.com>
2025-04-28 22:14:07 +0800

    add f5-tts to PYTHONPATH

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source activate base
+#source activate base
 conda activate latentsync
-export PYTHONPATH=$PYTHONPATH:./submodules/LatentSync
+export PYTHONPATH=$PYTHONPATH:./submodules/LatentSync:./submodules/F5-TTS/src
 python gradio_app.py
 conda deactivate
